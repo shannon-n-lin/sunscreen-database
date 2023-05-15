d108ed2ae92447c5cbaa1a786ae91178966f8c71
Shannon Lin <shannon.n.lin@gmail.com>
2023-05-15 14:34:30 -0700

    Add hosting environment port option

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.get('/api/:query', (req, res) => {
   }
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`The server is running on port ${PORT}.`)
 })
