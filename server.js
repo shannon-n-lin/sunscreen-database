484290d8ed29741d6b3a286c38e6057854822878
Shannon Lin <shannon.n.lin@gmail.com>
2023-05-18 12:00:50 -0700

    Add dotenv and MongoDB connection

--- server.js
+++ server.js
@@ -2,42 +2,34 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const PORT = 9000
+const dotenv = require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient
+const connectionString = `mongodb+srv://shannonnlin:${process.env.MONGODB_PASSWORD}@cluster1.yuwebwh.mongodb.net/?retryWrites=true&w=majority`
 
 app.use(cors())
-
-const sunscreens = {
-  'supergoop unseen sunscreen': {
-    'brand': 'supergoop!',
-    'name': 'unseen sunscreen',
-    'spf': 40,
-    'imageURL': 'https://supergoop.com/_cdn/A5I9PbaFjQo69uIottl6Hz/output=format:webp/cache=expiry:max/resize=width:1080%2cfit:max/quality=value:75/__N__1/https%3A/cdn.shopify.com/s/files/1/1503/5658/products/supergoop-unseen-sunscreen-spf-40-50ml.jpg?v=1623708290',
-  },
-  'beauty of joseon relief sun': {
-    'brand': 'beauty of joseon',
-    'name': 'relief sun: rice + probiotics',
-    'spf': 50,
-    'imageURL': 'https://i.ebayimg.com/images/g/8AMAAOSw~dNjo4JG/s-l1600.png'
-  },
-  'unknown': {
-    'brand': 'unknown',
-    'name': 'unknown',
-    'spf': 'unknown',
-    'imageURL': 'unknown'
-  },
-}
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/:query', (req, res) => {
-  let query = req.params.query.toLowerCase()
-  if (sunscreens[query]) {
-    res.json(sunscreens[query])
-  } else {
-    res.json(sunscreens['unknown'])
-  }
+MongoClient.connect(connectionString, {useUnifiedTopology: true, useNewUrlParser: true})
+  .then(client => {
+    console.log('Connected to database')
+    const db = client.db('sunscreen-database')
+    const infoCollection = db.collection('sunscreens')
+    
+    app.get('/api/:query', (req, res) => {
+      const query = req.params.query.toLowerCase()
+      infoCollection.find({name: query}).toArray()
+      .then(result => {
+        console.log(results)
+        res.json(results[0])
+      })
+      .catch(error => console.error(error))
+    })
 })
+.catch (error => console.error(error))
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`The server is running on port ${PORT}.`)
