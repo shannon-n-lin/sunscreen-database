ef6a892b797e9bd9a40cc11a82106b4c9f75e753
Shannon Lin <shannon.n.lin@gmail.com>
2023-05-19 10:41:22 -0700

    Add database autocomplete search

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,60 @@
-document.querySelector('button').addEventListener('click', apiRequest)
+// let host = 'https://sunscreen-database.onrender.com'
+let host = 'http://localhost:9000'
 
-async function apiRequest() {
-  const query = document.querySelector('input').value
+// jquery autocomplete search of database
+$(document).ready(function () {
+  $('#name').autocomplete({   
+    source: async function(req, res) {
+      let data= await fetch(`${host}/search?query=${req.term}`)
+        .then(results => results.json())
+        .then(results => results.map(result => {
+          return {
+            label: result.name,
+            value: result.name,
+            id: result._id,
+          }
+        }))
+        res(data)
+    }, 
+// load selected autocomplete search result
+    minLength: 2,
+    select: function(event, ui) {
+      fetch(`${host}/get/${ui.item.id}`)
+        .then(result => result.json())
+        .then(result => {
+          $('#brand').text(`Brand: ${result.brand}`)
+          $('#name').text(`Name: ${result.sunscreenName}`)
+          $('#spf').text(`SPF: ${result.spf}`)
+          $('#form').text(`Form: ${result.form}`)
+          $('#type').text(`Type: ${result.type}`)
+          $('#finish').text(`Finish: ${result.finish}`)
+          $('#price').text(`Price: $${(Number(result.priceUSD) / Number(result.ounces)).toFixed(2)} / ounce`)
+          $('#ingredientsLabel').text('Ingredients:')
+          $('#ingredients').empty()
+          result.ingredients.forEach(ing => {
+            $('#ingredients').append(`<li>${ing}</li>`)
+          })
+          $('img').attr('src', result.imageURL)
+          console.log(result)
+        })
+    }
+  })
+})
+
+// manual search
+document.querySelector('button').addEventListener('click', function() {
+  apiRequest(document.querySelector('input').value)
+})
+window.addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
+		apiRequest(document.querySelector('input').value)
+	}
+})
+
+// manual search API request
+async function apiRequest(search) {
   try {
-    // const res = await fetch(`https://sunscreen-database.onrender.com/api/${query}`)
-    const res = await fetch(`http://localhost:9000/api/${query}`)
+    const res = await fetch(`${host}/api/${search}`)
     const data = await res.json()
 
     console.log(data)
@@ -12,7 +62,7 @@ async function apiRequest() {
     document.getElementById('name').innerText = `Name: ${data.sunscreenName}`
     document.getElementById('spf').innerText = `SPF: ${data.spf}`
     document.getElementById('form').innerText = `Form: ${data.form}`
-    document.getElementById('type').innerText = `Type: ${data.spf}`
+    document.getElementById('type').innerText = `Type: ${data.type}`
     document.getElementById('finish').innerText = `Finish: ${data.finish}`
     let price = Number(data.priceUSD) / Number(data.ounces)
     document.getElementById('price').innerText = `Price: $${price.toFixed(2)} / ounce`
--- server.js
+++ server.js
@@ -1,37 +1,77 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { MongoClient, ObjectId } = require('mongodb')
 const PORT = 9000
-const dotenv = require('dotenv').config()
-const MongoClient = require('mongodb').MongoClient
-const connectionString = `mongodb+srv://shannonnlin:${process.env.MONGODB_PASSWORD}@cluster1.yuwebwh.mongodb.net/?retryWrites=true&w=majority`
+require('dotenv').config()
 
+// middleware
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
 
+// root page
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-MongoClient.connect(connectionString, {useUnifiedTopology: true, useNewUrlParser: true})
+// MongoDB connection
+let db, collection
+MongoClient.connect(process.env.DB_CONNECTION_STRING, {useUnifiedTopology: true, useNewUrlParser: true})
   .then(client => {
-    console.log('Connected to database')
-    const db = client.db('sunscreen-database')
-    const infoCollection = db.collection('sunscreens')
-    
-    app.get('/api/:query', (req, res) => {
-      const query = req.params.query.toLowerCase()
-      infoCollection.find({name: query}).toArray()
-      .then(results => {
-        console.log(results)
-        res.json(results[0])
-      })
-      .catch(error => console.error(error))
-    })
+    console.log('Connected to database.')
+    db = client.db('sunscreen-database')
+    collection = db.collection('sunscreens')
   })
-  .catch (error => console.error(error))
+  .catch(error => console.error(error))
 
+// jquery autocomplete search of database
+app.get('/search', async (req, res) => {
+  try {
+    let result = await collection.aggregate([
+      {
+        '$search': {
+          'autocomplete': {
+            'query': `${req.query.query}`,
+            'path': 'name',
+            'fuzzy': {
+              'maxEdits': 2,
+              'prefixLength': 3,
+            }
+          }
+        }
+      }
+    ]).toArray()
+    res.send(result)
+  } catch(error) {
+    res.status(500).send({message: error.message})
+  }
+})
+
+// load selected autocomplete search result
+app.get('/get/:id', async (req, res) => {
+  try {
+    let result = await collection.findOne({
+      '_id': ObjectId(req.params.id)
+    })
+    res.send(result)
+  } catch(error) {
+    res.status(500).send({message: error.message})
+  }
+})
+
+// manual search
+app.get('/api/:query', (req, res) => {
+  const query = req.params.query.toLowerCase()
+  collection.find({name: query}).toArray()
+  .then(results => {
+    console.log(results)
+    res.json(results[0])
+  })
+  .catch(error => console.error(error))
+})
+
+// connect to port in hosting environment or localhost
 app.listen(process.env.PORT || PORT, () => {
-  console.log(`The server is running on port ${PORT}.`)
+  console.log(`The server is running.`)
 })
